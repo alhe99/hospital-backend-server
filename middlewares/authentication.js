b0940c345e1a48018474dc6206348c9bed2af7ff
Luis Alonso Hernández <luisalonsohdezz@gmail.com>
2020-06-01 21:49:58 -0600

    Update Middleware Auth

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -2,8 +2,11 @@ var jwt = require("jsonwebtoken");
 var SEED = require("../config/config").SEED;
 
 exports.verifyToken = function (request, response, next) {
-  var token = request.query.token;
+  if (!request.headers.authorization) {
+    return response.status(403).send({ message: "Authentication is required" });
+  }
 
+  var token = request.headers.authorization.split(" ")[1];
   jwt.verify(token, SEED, (err, decoded) => {
     if (err) {
       return response.status(401).json({
